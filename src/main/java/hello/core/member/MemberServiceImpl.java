b88c264b4146f7c5b18eec881d3cc88b2c7e2544
Leegeonwoo7 <workgeon07@naver.com>
2024-03-10 20:12:04 +0900

    ref: AppConfig를 통해 DIP를 지킨다

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,7 +1,12 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    //S 생성자를 통해서 어떤 구현체를 받을지 결정한다
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
