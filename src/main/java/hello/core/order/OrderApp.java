f54de082ef81a71bbed3f5dc1a47bf5b1f5ec7f5
Leegeonwoo7 <workgeon07@naver.com>
2024-03-11 21:41:14 +0900

    ref: 스프링으로 변경

--- src/main/java/hello/core/order/OrderApp.java
+++ src/main/java/hello/core/order/OrderApp.java
@@ -3,18 +3,25 @@ package hello.core.order;
 import hello.core.AppConfig;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "hong", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
         System.out.println(order);
         System.out.println(order.calculatePrice());
 
