89a935e3d0cb21281022a082d168a972b52a993d
Leegeonwoo7 <workgeon07@naver.com>
2024-03-15 22:47:11 +0900

    test: XML로 빈 설정하기

---
+++ src/test/java/hello/core/xml/XmlAppcontextTest.java
@@ -0,0 +1,27 @@
+package hello.core.xml;
+
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.order.OrderService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class XmlAppcontextTest {
+    ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+    @Test
+    void xmlAppContext(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+    @Test
+    void xmlTest(){
+        OrderService orderService = ac.getBean(OrderService.class);
+        assertThat(orderService).isInstanceOf(RateDiscountPolicy.class);
+    }
+}
