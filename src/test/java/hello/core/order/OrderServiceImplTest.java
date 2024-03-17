be6d91cfd03fbc66d53ce4cf2c5828ef8b197b58
Leegeonwoo7 <workgeon07@naver.com>
2024-03-17 23:04:44 +0900

    test: 생성자 주입을 사용하였을 때 순수 자바코드로 테스트의 유용성 확인

---
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,25 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class OrderServiceImplTest {
+    @Test
+    @DisplayName("생성자 주입을 했을 때 테스트의 유용성 확인")
+    void createOrder(){
+        MemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
